cb8ee502fbebb378de0ede368b699600257adfe1
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-03-12 11:17:25 +0200

    add animation to splash view body

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,26 @@
 import 'package:booky_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<Offset> slidingLogoAnimation;
+  @override
+  void initState() {
+    super.initState();
+
+    sliding();
+    
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +28,48 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
+        AnimatedBuilder(
+          animation: slidingLogoAnimation,
+          builder: (context, _) {
+            return SlideTransition(
+              position: slidingLogoAnimation,
+              child: Image.asset(AssetsData.logo),
+            );
+          },
+        ),
         const SizedBox(
           height: 8,
         ),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, _) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                'Read Free Books',
+                textAlign: TextAlign.center,
+              ),
+            );
+          },
         ),
       ],
     );
+  }
+
+  
+  void sliding() {
+     animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 4),
+      end: Offset.zero,
+    ).animate(animationController);
+    slidingLogoAnimation = Tween<Offset>(
+      begin: const Offset(1, 0),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
   }
 }
