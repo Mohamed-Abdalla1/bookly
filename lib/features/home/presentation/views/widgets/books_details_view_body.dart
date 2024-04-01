b3c4e39794122da422b7063077d44b9631b13f1f
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-04-01 13:44:34 +0200

    add similar list view to book details body

--- lib/features/home/presentation/views/widgets/books_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/books_details_view_body.dart
@@ -1,9 +1,9 @@
 import 'package:booky_app/core/utils/styles.dart';
-import 'package:booky_app/core/widgets/custom_button.dart';
 import 'package:booky_app/features/home/presentation/views/widgets/book_action.dart';
 import 'package:booky_app/features/home/presentation/views/widgets/book_details_app_bar.dart';
 import 'package:booky_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:booky_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:booky_app/features/home/presentation/views/widgets/similiar_list%20view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -13,57 +13,71 @@ class BookDetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 28.0),
-      child: Column(
-        children: [
-          const BookDetailsAppBAr(),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * .27,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-              child: const CustomImage(),
-            ),
-          ),
-          const SizedBox(
-            height: 45,
-          ),
-          const Text(
-            'The Jungle Book',
-            style: Styles.textSize30,
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              'mark ssoemjas',
-              style: Styles.textSize18.copyWith(fontStyle: FontStyle.italic),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          const BookAction(),
-          const SizedBox(
-            height: 40,
-          ),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textSize18,
-            ),
-          ),
-          Similar
-        ],
-      ),
-    );
+        padding: const EdgeInsets.symmetric(horizontal: 28.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Column(
+                children: [
+                  const BookDetailsAppBAr(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .27,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+                      child: const CustomImage(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 45,
+                  ),
+                  const Text(
+                    'The Jungle Book',
+                    style: Styles.textSize30,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Opacity(
+                    opacity: 0.7,
+                    child: Text(
+                      'mark ssoemjas',
+                      style: Styles.textSize18
+                          .copyWith(fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const BookRating(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const BookAction(),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'You can also like',
+                      style: Styles.textSize18,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const SimilarListview(),
+                  const Expanded(
+                      child: SizedBox(
+                    height: 50,
+                  ))
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
