da4563ba4dff8419844d86aa3a09f98c11615eee
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-04-11 10:07:50 +0200

    create newest books method

--- lib/features/home/presentation/manager/newest books/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest books/newest_books_cubit.dart
@@ -1,10 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:booky_app/core/errors/failures.dart';
 import 'package:booky_app/features/home/data/models/book_model/book_model.dart';
+import 'package:booky_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit({required this.homeRepo}) : super(NewestBooksInitial());
+
+  
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    return result.fold((failure) => failure, (books) => books);
+  }
 }
